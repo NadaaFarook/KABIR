42568654fb78234e1200bf98df7feead5e672f7d
Nada <nadaaofficial19@gmail.com>
2023-05-09 02:47:01 +0530

    navbar

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,13 @@
 "use client";
 import React, { useState } from "react";
-import {  Syne } from "next/font/google";
+import { Syne } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "./assets/logo.png";
 import Bot from "./assets/bot.png";
 import Pfp from "./assets/pfp.png";
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
+import Navbar from "./Navbar";
 
 const syne = Syne({
   subsets: ["latin"],
@@ -15,32 +16,30 @@ const syne = Syne({
 
 const Main = () => {
   const [hamMenu, setHamMenu] = useState(false);
-  
+
   return (
     <div
-      className={` min-h-screen min-w-screen bg-blue text-white px-10 md:px-20 lg:px-40 pt-5 md:pt-10 overflow-hidden`}
+      className={` min-h-screen min-w-screen bg-blue text-white px-10 lg:px-20 xl:px-40 pt-5 overflow-hidden`}
     >
-      <nav className="flex items-center justify-between">
-        <div className="flex gap-10">
-          <Link href="/" className="flex items-center gap-3">
-            <Image weight={20} height={20} alt="logo" src={Logo} /> Cyber Surakshit </Link>
-
-          <Link className="hidden sm:inline" href="/about"> About </Link>
-          <Link className="hidden sm:inline" href="/privacypolicy"> Privacy Policy </Link>
-        </div>
-
-        <button className="bg-black px-10 py-2 rounded-md hover:bg-opacity-60 duration-200 hidden sm:inline">
-          Chat on Whatsapp
-        </button>
-
-        <RxHamburgerMenu onClick={() => setHamMenu(true)} className="sm:hidden text-4xl cursor-pointer hover:bg-black hover:bg-opacity-50 p-1 rounded-sm duration-200" />
-      </nav>
+      <Navbar isWhite={true} />
 
       <div
-        className={`absolute w-full top-0 left-0 bg-white text-black px-5 py-3  flex-col gap-3 ${ hamMenu ? "flex" : "hidden"}`}>
-        <RxCross1 onClick={() => setHamMenu(false)} className="absolute top-3 right-5 cursor-pointer" />
-        <Link className="block" href="/about">  About </Link>
-        <Link className="block" href="/privacypolicy"> Privacy Policy </Link>
+        className={`absolute w-full top-0 left-0 bg-white text-black px-5 py-3  flex-col gap-3 ${
+          hamMenu ? "flex" : "hidden"
+        }`}
+      >
+        <RxCross1
+          onClick={() => setHamMenu(false)}
+          className="absolute top-3 right-5 cursor-pointer"
+        />
+        <Link className="block" href="/about">
+          {" "}
+          About{" "}
+        </Link>
+        <Link className="block" href="/privacypolicy">
+          {" "}
+          Privacy Policy{" "}
+        </Link>
 
         <button className="bg-black px-3 py-2 rounded-md hover:bg-opacity-60 duration-200 w-1/2 text-white">
           Chat on Whatsapp
